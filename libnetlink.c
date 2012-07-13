1e48585a6faab0ab24c89b84ae79fd741510a2fb
Auke Kok <auke-jan.h.kok@intel.com>
2012-07-12 21:50:07 -0700

    bugfix: incorrect sizeof() usage for memset.
    This ends up memsetting the size of a pointer instead of the
    entire struct.
    
    Signed-off-by: Auke Kok <auke-jan.h.kok@intel.com>

--- libnetlink.c
+++ libnetlink.c
@@ -41,7 +41,7 @@ int rtnl_open_byproto(struct rtnl_handle *rth, unsigned subscriptions,
 	int sndbuf = 32768;
 	int rcvbuf = 32768;
 
-	memset(rth, 0, sizeof(rth));
+	memset(rth, 0, sizeof(struct rtnl_handle));
 
 	rth->fd = socket(AF_NETLINK, SOCK_RAW | SOCK_CLOEXEC, protocol);
 	if (rth->fd < 0) {
