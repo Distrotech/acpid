3559820918b38427333ca8f3eb1a6bef7b7a8222
Ted Felix <ted@tedfelix.com>
2014-04-29 20:14:32 -0400

    Avoid using KEY_MICMUTE if it doesn't exist
    Patch from Lonnie Abelbeck.  Closes ticket #2.

--- input_layer.c
+++ input_layer.c
@@ -112,8 +112,11 @@ static struct evtab_entry evtab[] = {
  		"button/volumeup VOLUP 00000080 00000000"},
 	{{{0,0}, EV_KEY, KEY_MUTE, 1},
  		"button/mute MUTE 00000080 00000000"},
+/* Kernel 3.1 or later required for KEY_MICMUTE */
+#ifdef KEY_MICMUTE
 	{{{0,0}, EV_KEY, KEY_MICMUTE, 1},
 		"button/micmute MICMUTE 00000080 00000000"},
+#endif
  	/* cd play/pause buttons */
  	{{{0,0}, EV_KEY, KEY_NEXTSONG, 1},
  		"cd/next CDNEXT 00000080 00000000"},
